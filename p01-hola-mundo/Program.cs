bdd936e09000a1234c460e2f996206131b0fd735
Violeta CD <31123695@uaz.edu.mx>
2023-02-09 10:56:37 -0600

    primero

---
+++ p01-hola-mundo/Program.cs
@@ -0,0 +1,12 @@
+﻿// envia un saludo a la pantalla
+
+string amigo="JUAN";
+string hermano="Pablo";
+
+
+Console.WriteLine(amigo);
+amigo="MIGUEL";
+Console.WriteLine(amigo);
+Console.WriteLine($"HOLA {amigo} bienvenido a C#");
+Console.WriteLine($"\n Mi amigo {amigo} mi hermano {hermano}\n");
+Console.WriteLine($"{amigo.ToUpper()} {hermano.ToUpper()}");
