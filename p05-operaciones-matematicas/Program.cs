a284b19c7ad75b043499d1812b5403bbf0ec0a6a
Violeta CD <31123695@uaz.edu.mx>
2023-02-14 11:58:16 -0600

    acividad 14/02/2023

---
+++ p05-operaciones-matematicas/Program.cs
@@ -0,0 +1,16 @@
+﻿// Efectua operaciones matematicas con dos numeros
+double x, y, suma, resta, mult, div, res, pot;
+Console.WriteLine("Efectuando operaciones matematicas con dos numeros:\n");
+x = 10.5 ;
+y = 2.5 ;
+suma = x + y ;
+resta = x - y ;
+mult = x * y ;
+div = x / y ;
+res = x % y ;
+pot = Math.Pow(x, y) ;
+Console.WriteLine($"Los numeros son: {x}, {y}\n");
+Console.WriteLine("El resultado de las operaciones es el siguiente:\n");
+Console.WriteLine($"Suma: {suma}\nResta: {resta}");
+Console.WriteLine($"Multiplicacion: {mult}\nDivision: {div}");
+Console.WriteLine($"Residuo: {res}\nPotencia: {pot}");
---
+++ p08-funciones-trigonometricas/Program.cs
@@ -0,0 +1,17 @@
+﻿// Calcula funciones trigonometricas de un angulo en radianas
+Console.Clear();
+Console.WriteLine("Calculando las funciones trigonometricas de un angulo en radianes: \n");
+
+Console.Write("Dame un angulo en Radianes: ");
+double angulo = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"El angulo {angulo} tiene las siguientes funciones trigonometricas: \n");
+Console.WriteLine("Seno: {0:f4}, Coseno: {1:f4}, tangente: {2:f4}",Math.Sin(angulo),Math.Cos(angulo),Math.Tan(angulo));
+
+//double seno=Math.Sin(angulo);
+//double coseno=Math.Cos(angulo);
+//double tangente=Math.Tan(angulo);
+//Console.WriteLine($"El Seno : {seno} ");
+//Console.WriteLine($"El Coseno : {coseno} ");
+//Console.WriteLine($"La Tangente : {tangente} ");
