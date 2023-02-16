f69f852497924f3e614825ab605a4bf6e35383f3
Violeta CD <31123695@uaz.edu.mx>
2023-02-16 11:52:31 -0600

    Acividad  3

---
+++ p10-trabajando-enteros/Program.cs
@@ -0,0 +1,29 @@
+﻿// Trabajando con numeros enteros
+
+int num1 = 42;
+int num2 = 0x2A;
+int num3 = 0b00101010;
+
+Console.Clear();
+Console.WriteLine($"Decimal {num1} {num2} {num3}");
+Console.WriteLine($"num1 en base 10: {num1}");
+Console.WriteLine($"num1 en base 8: {Convert.ToString(num1,8)}");
+Console.WriteLine($"num1 en base 16: {Convert.ToString(num1,16)}");
+Console.WriteLine($"num1 en base 2: {Convert.ToString(num1,2)}");
+
+sbyte edad = 95;
+byte val1 = 230, val2 = 254;
+short horas = 24, dias = 12345;
+long ganancia = 123456789000334124;
+ulong añosluz = 10000000000000000000;
+
+Console.WriteLine($"La ganancia anual es: {ganancia:c}");
+Console.WriteLine($"Años luz a la luna: {añosluz:e}");
+Console.WriteLine($"num2 base dec,hex: {num2:d}, {num2:x}");
+
+Console.WriteLine($"sbyte, {sbyte.MinValue} , {sbyte.MaxValue}");
+Console.WriteLine($"byte, {byte.MinValue} , {byte.MaxValue}");
+Console.WriteLine($"int, {int.MinValue} , {int.MaxValue}");
+Console.WriteLine($"short, {short.MinValue} , {short.MaxValue}");
+Console.WriteLine($"long, {long.MinValue} , {long.MaxValue}");
+Console.WriteLine($"ulong, {ulong.MinValue} , {ulong.MaxValue}");
---
+++ p12-trabajando-constantes/Program.cs
@@ -0,0 +1,19 @@
+﻿//Trabajando constantes
+
+const double DESCUENTO = 0.10;
+const int VELOCIDADLUZ = 30000;
+const float TASA = 0.5f;
+const byte SEPARADOR = 254;
+const sbyte LITERAL = 0x64;
+const string MENSAJE = "ERROR EN EL SISTEMA, TODA A EXPLOTAR en 5 4 3 2 1 0";
+
+Console.Clear();
+Console.WriteLine("El valor de las constantes es: \n");
+Console.WriteLine("descuento x 2 : {0}", 2 * DESCUENTO);
+Console.WriteLine("velocidad / 2 : {0}", VELOCIDADLUZ / 2);
+Console.WriteLine("tasa : {0:p}", TASA);
+
+Console.WriteLine(MENSAJE);
+Console.WriteLine("separador - literal en hexadecimal : {0:x} - {1:x}", SEPARADOR, LITERAL);
+Console.WriteLine("separador - literal en binario : {0} - {1}",Convert.ToString(SEPARADOR,2) , Convert.ToString(LITERAL,2) );
+
