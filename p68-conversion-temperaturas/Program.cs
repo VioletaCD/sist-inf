7f079007a07a2ae33248d1ca040564065eb176ff
Violeta CD <31123695@uaz.edu.mx>
2023-03-28 11:34:02 -0600

    Actividad 12 - Funciones 28/03/23

--- p68-conversion-temperaturas/Program.cs
+++ p68-conversion-temperaturas/Program.cs
@@ -1,23 +1,34 @@
-﻿// Conversion de temperaturas usando funciones
-float Farenheit(float t) {
-return (t*9/5)+32;
+﻿// Conversion de temperaturas usando funciones 
+float farenheit(float t){
+    return (t*9/5)+32;
 }
-float Celcius(float t) {
-return (t-32)*5/9;
+float celcius(float t){
+    return (t-32)*5/9;
 }
+
+//Programa principal
 int op;
 float temp, res;
+
 Console.Clear();
-Console.Write("[1] farenheit\n[2] celcius\nElige ? ");
-op = int.Parse(Console.ReadLine());
-Console.Write("Dame la temperatura ? ");
-temp = float.Parse(Console.ReadLine());
-if(op==1) {
-res = Farenheit(temp);
-Console.WriteLine($"\n{temp} grados celcius equivale a {res} grandos farenheit ..");
+Console.WriteLine("Conversion de temperatura usando funciones \n");
+Console.WriteLine("[1] Farenheit \n[2]Centigrados");
+Console.Write("Elige ? "); op = int.Parse(Console.ReadLine());
+
+switch(op){
+    case 1:
+    Console.Write("Dame la temperatura en centigrados ? ");
+    temp = float.Parse(Console.ReadLine());
+    res = farenheit(temp);
+    Console.WriteLine($"\n{temp} grados centigrados equivale a {res} grados farenheit");
+    break;
+    case 2:
+    Console.Write("Dame la temperatura en Farenheit ?");
+    temp = float.Parse(Console.ReadLine());
+    res = celcius(temp);
+    Console.WriteLine($"{temp} grados farenheit equivale a {res} grados celcius");
+    break;
+    default:
+    Console.WriteLine("Opcion invalida");
+    break;
 }
-else if(op==2) {
-res = Celcius(temp);
-Console.WriteLine($"\n{temp} grados farenheit equivale a {res} grandos celcius ..");
-}
-else Console.WriteLine("Opción inválida");
